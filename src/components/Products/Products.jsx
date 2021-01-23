5f0401900a9b113a7d1b34e17825d86c414b443a
Amiinoz <momagan@gmail.com>
2021-01-22 21:39:38 -0700

    Finishing products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product'
 
+
+
 const products = [
-  { id:1, name:'tees', description:'mmanro.', price:'$55'},
-  { id:2, name:'shorts', description:'bnw.', price:'$67'},
+  { id:1, name:'tees', description:'ain.', price:'$55', image:'https://unsplash.com/photos/RTr1c-ZLe0c', },
+  // { id:1, name:'tees', description:'ain.', price:'$55', image:'/src/images/ain.jpeg', },
+  { id:2, name:'tees', description:'ayo.', price:'$39', image: '/src/images/ayo.jpeg'},
+  { id:3, name:'tees', description:'bant.', price:'$55', image: '/src/images/bant.jpg'},
+  { id:4, name:'tees', description:'gal.', price:'$45', image: '/src/images/gal.jpeg'},
+  { id:5, name:'tees', description:'jul.', price:'$55', image: '/src/images/jul.jpg'},
+  { id:6, name:'tees', description:'taller.', price:'$67', image: '/src/images/taller.jpeg'},
 ]
 
 const Products = () => {
